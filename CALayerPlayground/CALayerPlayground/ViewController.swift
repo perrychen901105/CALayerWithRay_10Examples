25660f77b7fb942b68fdffa40bb244f641f93c1a
Perry <chenzhipeng901105@gmail.com>
2015-01-29 14:43:26 +0800

    add omg

--- CALayerPlayground/CALayerPlayground/ViewController.swift
+++ CALayerPlayground/CALayerPlayground/ViewController.swift
@@ -28,13 +28,31 @@ class ViewController: UIViewController {
         l.borderColor = UIColor.redColor().CGColor
         l.shadowOpacity = 0.7
         l.shadowRadius = 10.0
+//        l.masksToBounds = true
+        l.contents = UIImage(named: "star")?.CGImage
+        l.contentsGravity = kCAGravityCenter
+    }
+
+    @IBAction func tapGestureRecognized(sender: UITapGestureRecognizer) {
+        
+        l.shadowOpacity = l.shadowOpacity == 0.7 ? 0.0 : 0.7
+    }
+   
+    @IBAction func pinchGestureRecognized(sender: UIPinchGestureRecognizer) {
+        let offset: CGFloat = sender.scale < 1 ? 5.0 : -5.0
+        let oldFrame = l.frame
+        let oldOrigin = oldFrame.origin
+        let newOrigin = CGPoint(x: oldOrigin.x + offset, y: oldOrigin.y + offset)
+        let newSize = CGSize(width: oldFrame.width + (offset * -2.0), height: oldFrame.height + (offset * -2.0))
+        let newFrame = CGRect(origin: newOrigin, size: newSize)
+        if newFrame.width >= 100.0 && newFrame.width <= 300 {
+            l.borderWidth -= offset
+            l.cornerRadius += (offset / 2.0)
+            l.frame = newFrame
+        }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
 
